ba7ef3369fe3c8b4e07c9392acc44c2621b95d5c
Carlos Trujillo <carlos.trujillo.bazante270494@gmail.com>
2024-12-11 23:41:06 -0500

    Comentarios falts

--- src/components/Messages/MessageList.jsx
+++ src/components/Messages/MessageList.jsx
@@ -49,25 +49,27 @@ function MessageList({ flat }) {
           <article key={index} className="flex items-start gap-2.5">
             <img
               className="w-8 h-8 rounded-full"
-              src="/docs/images/people/profile-picture-3.jpg"
+              src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png"
               alt="Jese image"
             />
             <div className="flex flex-col gap-1 w-full max-w-[320px]">
               <div className="flex items-center space-x-2 rtl:space-x-reverse">
                 <span className="text-sm font-semibold text-gray-900 dark:text-white">
-                  {item.uid}
+                  {"Marlos"}
                 </span>
                 <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                  {item.date}
+                  {new Date(item.created).toLocaleTimeString() +
+                    " " +
+                    new Date(item.created).toDateString()}
                 </span>
               </div>
               <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-indigo-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
                 <p className="text-sm font-normal text-gray-900 dark:text-white">
-                  {item.text}
+                  {item.content}
                 </p>
               </div>
               <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                Delivered2
+                Delivered
               </span>
             </div>
           </article>
